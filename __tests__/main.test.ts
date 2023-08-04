0973cbead024e43fab1fb45c32942d8c73c0e12f
Patrick Dawson <patrick.dawson79@gmail.com>
2023-08-04 10:43:16 +0200

    Add test for jest isolateModulesAsync bug

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,42 +1,18 @@
-import { Delays, greeter } from '../src/main.js';
+import {jest} from "@jest/globals";
 
-describe('greeter function', () => {
-  const name = 'John';
-  let hello: string;
-
-  let timeoutSpy: jest.SpyInstance;
-
-  // Act before assertions
-  beforeAll(async () => {
-    // Read more about fake timers
-    // http://facebook.github.io/jest/docs/en/timer-mocks.html#content
-    // Jest 27 now uses "modern" implementation of fake timers
-    // https://jestjs.io/blog/2021/05/25/jest-27#flipping-defaults
-    // https://github.com/facebook/jest/pull/5171
-    jest.useFakeTimers();
-    timeoutSpy = jest.spyOn(global, 'setTimeout');
-
-    const p: Promise<string> = greeter(name);
-    jest.runOnlyPendingTimers();
-    hello = await p;
-  });
-
-  // Teardown (cleanup) after assertions
-  afterAll(() => {
-    timeoutSpy.mockRestore();
-  });
-
-  // Assert if setTimeout was called properly
-  it('delays the greeting by 2 seconds', () => {
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(
-      expect.any(Function),
-      Delays.Long,
-    );
-  });
-
-  // Assert greeter result
-  it('greets a user with `Hello, {name}` message', () => {
-    expect(hello).toBe(`Hello, ${name}`);
+describe('jest isolateModulesTest', () => {
+  it('should have a fresh module state in each isolateModulesAsync context', async () => {
+    await jest.isolateModulesAsync(async () => {
+      const { getState, incState } = await import('../src/main.js');
+      expect(getState()).toBe(0);
+      incState();
+      expect(getState()).toBe(1);
+    });
+    await jest.isolateModulesAsync(async () => {
+      const { getState, incState } = await import('../src/main.js');
+      expect(getState()).toBe(0);
+      incState();
+      expect(getState()).toBe(1);
+    });
   });
 });
